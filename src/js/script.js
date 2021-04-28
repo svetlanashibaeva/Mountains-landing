0b086edc690bdbe5b24f4878419d4dfc856612a7
Shibaeva Svetlana <svetlanashibaeva96@gmail.com>
2021-04-28 17:03:35 +0300

    добавлен эффект параллакса на фон промо блока

--- src/js/script.js
+++ src/js/script.js
@@ -12,3 +12,31 @@
 //     $('.mg > h1').css('top',-(scrolled*-0.005)+'rem');
 //     $('.mg > h1').css('opacity',1-(scrolled*.00175));
 //   };
+
+var image = document.querySelector('.hg');
+var image2 = document.querySelector('.mg');
+
+new simpleParallax(image, {
+    overflow: true,
+    scale: 1,
+    delay: .6,
+    transition: 'cubic-bezier(0,0,0,1)'
+});
+
+new simpleParallax(image2, {
+    overflow: true,
+    delay: .6,
+    transition: 'cubic-bezier(0,0,0,1)'
+});
+
+var image3 = document.querySelector('.vg');
+
+new simpleParallax(image3, {
+    overflow: true
+});
+
+// var images = document.querySelectorAll('.promo__image');
+
+// new simpleParallax(images, {
+//     overflow: true
+// });
